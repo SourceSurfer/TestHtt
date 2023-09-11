ff289e2030a70ce491da1525f5188c1e49be4898
Mark <koly86@gmail.com>
2023-09-11 03:06:09 +0300

    FirstCommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestHtt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
